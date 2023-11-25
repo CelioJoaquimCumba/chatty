6d12df01e7a683dc57e05c7725d366725faf514e
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-25 08:02:37 +0200

    refactor: change style

--- src/components/atoms/ChatBubble.tsx
+++ src/components/atoms/ChatBubble.tsx
@@ -7,14 +7,14 @@ type ChatBubbleProps = {
 
 export const ChatBubble = ({isOwner, message, sender, timestamp}: ChatBubbleProps) => {
     return(
-        <div className={`w-full self-stretch px-4 py-0.5 bg-background rounded-lg justify-start items-center gap-2 flex text-justify ${isOwner ? "flex-row-reverse text-end" : "flex-row text-start"} break-all`}>
+        <div className={` max-w-[75%] px-4 py-0.5 bg-background rounded-lg justify-start items-center gap-2 flex text-justify ${isOwner ? "flex-row-reverse text-end  self-end bg-primary text-background" : "flex-row text-start bg-secondary  self-start text-foreground"} break-all`}>
             <img className="w-10 h-10 rounded-lg border border-gray-300" src="https://via.placeholder.com/40x40" />
-            <div className={`self-stretch w-4/6  py-4 flex-col gap-2 flex overflow-hidden ${isOwner ? "items-end" : "items-start"}`}>
-                <div className="justify-end items-end gap-1 flex ">
-                    <div className="text-primary text-base font-semibold leading-normal">{isOwner ? "You" : sender}</div>
-                    <div className="text-foreground text-xs font-normal leading-none">{timestamp}</div>
+            <div className={`self-stretch py-4 flex-col gap-2 flex overflow-hidden ${isOwner ? "items-end" : "items-start"}`}>
+                <div className="justify-end items-center gap-2 flex flex-wrap ">
+                    <div className="text-base font-semibold leading-normal">{isOwner ? "You" : sender}</div>
+                    <div className=" text-xs font-normal leading-none">{timestamp}</div>
                 </div>
-                <div className="text-foreground text-sm font-normal leading-tight">{message}</div>
+                <div className="text-sm font-normal leading-tight">{message}</div>
             </div>
         </div>
     )
